f8382fc9cb55ef6d070b9e2414d1fef8f49047a9
testxxxxxxxxx <pppppmarcin@gmail.com>
2025-09-25 20:04:31 +0200

    created routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,13 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware("auth")->group(function() {
+    Route::controller(CardController::class)->group(function() {
+        Route::get("/getCard", "index")->name("index");
+        Route::get("/getAllCards", "show")->name("show");
+        Route::post("/createCard", "create")->name("create");
+        Route::post("/updateCard", "update")->name("update");
+        Route::post("/deleteCard", "delete")->name("delete");
+    });
+});
