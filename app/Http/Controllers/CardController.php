cb3c49d8eca15946b10474c52bdf02e74aab8763
testxxxxxxxxx <pppppmarcin@gmail.com>
2025-09-25 19:57:27 +0200

    CardController is done

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -2,12 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use App\Http\Requests\IDRequest;
+use App\Http\Requests\CardRequest;
 use App\Services\CardService;
+use Illuminate\View\View;
 
 class CardController extends Controller
 {
-    public function index() {
-
+    public function __construct(private CardService $cardService) {
+        $this->cardService = $cardService;
+    }
+    public function index(IDRequest $idRequest): View {
+        $id = $idRequest->input("id");
+        $card = $this->cardService->get($id);
+        $number = $card->number;
+        $PIN = $card->PIN;
+        $activate = $card->activate;
+        $validity = $card->validity;
+        $balance = $card->balance;
+        return view("index", ["number" => $number, "PIN" => $PIN, "activate" => $activate, "validity" => $validity, "balance" => $balance]);
+    }
+    public function show(): View {
+        $cards = $this->cardService->getAll();
+        return view("index", ["cards" => $cards]);
+    }
+    public function create(CardRequest $cardRequest): RedirectResponse {
+        $number = $cardRequest["number"];
+        $PIN = $cardRequest["PIN"];
+        $activate = $cardRequest["activate"];
+        $validity = $cardRequest["validity"];
+        $balance = $cardRequest["balance"];
+        $message = $this->cardService->create($number, $PIN, $activate, $validity, $balance)?"true":"false";
+        return redirect()->route("index", ["message" => $message]);
+    }
+    public function update(IDRequest $idRequest, CardRequest $cardRequest): RedirectResponse {
+        $id = $cardRequest["id"];
+        $number = $cardRequest["number"];
+        $PIN = $cardRequest["PIN"];
+        $activate = $cardRequest["activate"];
+        $validity = $cardRequest["validity"];
+        $balance = $cardRequest["balance"];
+        $message = $this->cardService->update($id, $number, $PIN, $activate, $validity, $balance)?"true":"false";
+        return redirect()->route("index", ["message" => $message]);
+    }
+    public function delete(IDRequest $idRequest): RedirectResponse {
+        $id = $idRequest["id"];
+        $message = $this->cardService->delete($id)?"true":"false";
+        return redirect()->route("index", ["message" => $message]);
     }
 }
