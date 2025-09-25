4a2354883b06373c744fbd53bdacfe7dc8c8e3a0
testxxxxxxxxx <pppppmarcin@gmail.com>
2025-09-25 12:37:36 +0200

    initial commit

---
+++ app/Models/Card.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Card extends Model
+{
+    protected $table = "cards";
+    protected $fillable = [
+        "number",
+        "PIN",
+        "activate",
+        "validity",
+        "balance"
+    ];
+}
