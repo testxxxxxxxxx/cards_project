3b91626bad7c0fd92ee2e93ec83289798ba9fe70
testxxxxxxxxx <pppppmarcin@gmail.com>
2025-09-26 22:14:42 +0200

    integrated vue with laravel

---
+++ resources/js/App.vue
@@ -0,0 +1,4 @@
+<template>
+<a href="login">Login</a>
+<a href="register">Register</a>
+</template>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-10">
+    <meta name="viewport" content="width=device-width, initial-scale=-1.0">
+    <title>Card app</title>
+    @vite(['resources/js/app.js'])
+</head>
+<body>
